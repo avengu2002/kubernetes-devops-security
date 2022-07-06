cd4109c05b0973c91133b002c8051aca904f8472
Your Name <you@example.com>
2022-07-06 12:49:01 +0530

    Updated script files for CIS Benchmark

--- cis-etcd.sh
+++ cis-etcd.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #cis-etcd.sh
 
-total_fail=$(kube-bench run --targets etcd  --version 1.20 --check 2.2 --json | jq .Totals.total_fail)
+#total_fail=$(kube-bench run --targets etcd  --version 1.20 --check 2.2 --json | jq .Totals.total_fail)
+total_fail=$(docker run --pid=host -v /etc:/etc:ro -v /var:/var:ro -v $(which kubectl):/usr/local/mount-from-host/bin/kubectl -v ~/.kube:/.kube -e KUBECONFIG=/.kube/config -t aquasec/kube-bench:latest  run --version 1.20 --targets node --check 2.2 --json | jq .Totals.total_fail)
 
 if [[ "$total_fail" -ne 0 ]];
         then
--- cis-master.sh
+++ cis-master.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #cis-master.sh
 
-total_fail=$(kube-bench master  --version 1.20 --check 1.2.7,1.2.8,1.2.9 --json | jq .Totals.total_fail)
+#total_fail=$(kube-bench master  --version 1.20 --check 1.2.7,1.2.8,1.2.9 --json | jq .Totals.total_fail)
+total_fail=$(docker run --pid=host -v /etc:/etc:ro -v /var:/var:ro -v $(which kubectl):/usr/local/mount-from-host/bin/kubectl -v ~/.kube:/.kube -e KUBECONFIG=/.kube/config -t aquasec/kube-bench:latest  run --version 1.20 --targets node --check 1.2.7,1.2.8,1.2.9 --json | jq .Totals.total_fail)
 
 if [[ "$total_fail" -ne 0 ]];
         then
